bda18667ff263673a155b797c36f089b9a22ab49
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-14 05:35:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express();
 const line = require('@line/bot-sdk');
+const Client = require('@line/bot-sdk').Client;
+const client = new Client({
+  channelAccessToken: 'dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
+});
+const event = req.body.events[0];
+if (event.type === 'message' && event.message.type === 'text') {
+      client.replyMessage(event.replyToken, {
+        type: 'text',
+        text:  event.message.text
+      });
+}else{
+      client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: 'Unknow command'
+      });
+}
 //const middleware = require('@line/bot-sdk').middleware
-const config = {
+/*const config = {
   channelAccessToken: 'dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=',
   channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
 }
@@ -15,21 +32,21 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     //return res.json({ status: 'ok' })
 });
 function handleEvent(event) {
- /* var echo;
+  var echo;
   if (event.type === 'message' || event.message.type === 'text') {
     echo = { 
         type: 'text', 
         text: 'Unknow input'
     };
-  }else{*/
+  }else{
   var  echo = { 
         type: 'text', 
         text: event.message.text 
     };
-//  }
+  }
 return client.replyMessage(event.replyToken, echo); 
 
-}
+}*/
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
