740aaa541c70ebf83f04de38d28af9dbf2211c59
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-13 21:46:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-'use strict';
+const express = require('express')
+const middleware = require('@line/bot-sdk').middleware
 
-const line = require('@line/bot-sdk');
-const express = require('express');
+const app = express()
 
-// create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
-
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret
 }
 
-// listen on port
+app.post('/webhook', middleware(config), (req, res) => {
+  res.json(req.body.events) // req.body will be webhook event object
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
