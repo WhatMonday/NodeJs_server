0de5edd609a19ae66f9dbe39e0c407261cc9856d
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-13 22:26:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const middleware = require('@line/bot-sdk').middleware
 const app = express()
 
 const config = {
-  channelAccessToken: 'dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=',
+  channelAccessToken: process.env.channelAccessToken,
   channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
 }
 
