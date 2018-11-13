d239b1cc668dcbec07074a26fd6748c0b02ab710
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-14 05:49:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const line = require('@line/bot-sdk');
 const Client = require('@line/bot-sdk').Client;
-const client = new Client({
+const config = {
   channelAccessToken: 'dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=',
   channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
-});
+}
+const client = new Client(config);
 app.post('/webhook', line.middleware(config), (req, res) => {
 const event = req.body.events[0];
 if (event.type === 'message' && event.message.type === 'text') {
