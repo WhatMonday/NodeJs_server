ae49a2cb549394faae7aab4844aa33c5bd1d71ab
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-13 16:57:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
-require('dotenv').config();
-const app = express();
-
-const config = {
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret
-};
-
-const client = new line.Client(config);
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-      .all(req.body.events.map(handleEvent))
-      .catch((e) => {
-          console.log(e)
-      })
-  return res.json({status: 'ok'})
-}
-
-// File:handler/index.js
-// Main logic to process event based on trigger message
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve('ok');
-  }
-
-/*function handleMessageEvent(event) {
-    var msg = {
-        type: 'text',
-        text: 'สวัสดีครัช'
-    };
-
-    return client.replyMessage(event.replyToken, msg);
-    
-}*/
-
-app.set('port', (process.env.PORT || 8080));
-app.listen(app.get('port'), function () {
-    console.log('run at port', app.get('port'));
+const http = require('http');
+const port = process.env.PORT || 8080
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+});
+server.listen(port, () => {
+    console.log(`Server running at port ` + port);
 });
