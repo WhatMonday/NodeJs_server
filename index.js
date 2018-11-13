d8e778927850bac4e576bf2972bc86a718b06035
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-14 06:13:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,47 +9,22 @@ const config = {
 const client = new Client(config);
 app.post('/webhook', line.middleware(config), (req, res) => {
 const event = req.body.events[0];
-if (event.type === 'message' && event.message.type === 'text') {
+  if (event.type === 'message' && event.message.type === 'text') {
+    var input = event.message.text;
+    var str = input.split(',');
+    var command = str[0];
+    var pin = str[1];
       client.replyMessage(event.replyToken, {
         type: 'text',
-        text:  event.message.text
+        text:  input
       });
-}else{
+    }else{
       client.replyMessage(event.replyToken, {
         type: 'text',
         text: 'Unknow command'
       });
 }
 });
-//const middleware = require('@line/bot-sdk').middleware
-/*const config = {
-  channelAccessToken: 'dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
-}
-app.post('/webhook', line.middleware(config), (req, res) => {
- Promise
-        .all(req.body.events.map(handleEvent))
-        .catch((e) => {
-            console.log(e)
-        })
-    //return res.json({ status: 'ok' })
-});
-function handleEvent(event) {
-  var echo;
-  if (event.type === 'message' || event.message.type === 'text') {
-    echo = { 
-        type: 'text', 
-        text: 'Unknow input'
-    };
-  }else{
-  var  echo = { 
-        type: 'text', 
-        text: event.message.text 
-    };
-  }
-return client.replyMessage(event.replyToken, echo); 
-
-}*/
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
