f037ca95ed4fa3083847cf94cdbb2df86e15f95e
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-13 22:24:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/webhook', middleware(config), (req, res) => {
   res.json(req.body.events) // req.body will be webhook event object
 })
 
-const port = process.env.port || '8080';
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
