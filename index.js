4c476063ae16a36a0ad220454cb02a3a51a08074
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-13 16:41:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const http = require('http');
-const port=process.env.PORT || 8080
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+const express = require('express');
+const line = require('@line/bot-sdk');
+require('dotenv').config();
+const app = express();
+
+const config = {
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret
+};
+
+const client = new line.Client(config);
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+
+function handleEvent(event) {
+
+    console.log(event);
+    if (event.type === 'message' && event.message.type === 'text') {
+        //handleMessageEvent(event);
+        let triggerMsg = event.message.text.toUpperCase()
+        if (triggerMsg === 'CHECK,0') {
+            var msg = {
+                type: 'text',
+                text: 'Hello World'
+            };
+
+            return client.replyMessage(event.replyToken, msg);
+        }
+        //return Promise.resolve('ok');
+    } else {
+        return Promise.resolve(null);
+    }
+}
+
+/*function handleMessageEvent(event) {
+    var msg = {
+        type: 'text',
+        text: 'สวัสดีครัช'
+    };
+
+    return client.replyMessage(event.replyToken, msg);
+    
+}*/
+
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'), function () {
+    console.log('run at port', app.get('port'));
 });
