608e2df61d06ae85458f01102ce47e1e1d98cc48
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-14 07:09:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,3 +29,16 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+//netpie microgear
+var MicroGear = require('microgear');
+const APPID = 'PocketBot';
+const KEY = 'E8d0mBCaYxpb6FW';
+const SECRET = 'XxnxMl4kZ51vWCli1rQpEtib7';
+var microgear = MicroGear.create({ key : KEY, secret : SECRET });
+microgear.on('connected', function() { console.log('Connected...');
+microgear.setAlias("nodejs");
+setInterval(function() { microgear.chat('mygear', 'Hello world.'); },1000); });
+microgear.on('message', function(topic,body) { console.log('incoming : '+topic+' : '+body); });
+microgear.on('closed', function() { console.log('Closed...'); });
+microgear.connect(APPID);
