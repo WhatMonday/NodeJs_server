b6426d5147847788ded1edbef63fe91139d8192c
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-14 12:21:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const config = {
   channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
 }
 const client = new Client(config);
-//netpie microgear
-var MicroGear = require('microgear');
-const APPID = 'PocketBot';
-const KEY = 'E8d0mBCaYxpb6FW';
-const SECRET = 'XxnxMl4kZ51vWCli1rQpEtib7';
-var microgear = MicroGear.create({ key : KEY, secret : SECRET });
-microgear.connect(APPID);
-microgear.on('connected', function() { console.log('Connected to NETPIE'); microgear.setAlias("nodejs");});
-microgear.on('message', function(topic,body) { console.log('incoming : '+topic+' : '+body); });
 app.post('/webhook', line.middleware(config), (req, res) => {
 const event = req.body.events[0];
   if (event.type === 'message' && event.message.type === 'text') {
@@ -29,8 +20,26 @@ const event = req.body.events[0];
       });
 }
 });
-
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
+});
+//netpie microgear=========================================================================================
+var MicroGear = require('microgear');
+const APPID = 'PocketBot';
+const KEY = 'E8d0mBCaYxpb6FW';
+const SECRET = 'XxnxMl4kZ51vWCli1rQpEtib7';
+var microgear = MicroGear.create({ key : KEY, secret : SECRET });
+microgear.connect(APPID);
+microgear.on('connected', function() { console.log('Connected to NETPIE'); microgear.setAlias("nodejs");});
+
+// message handler
+microgear.on('message', function(topic,body) { 
+  console.log('incoming : '+topic+' : '+body); 
+  
+
+
+
+
+
 });
