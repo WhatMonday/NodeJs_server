34e6d98978571873b4792e0e55eac05153f2ffe9
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-15 18:47:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = express();
 const line = require('@line/bot-sdk');
 const Client = require('@line/bot-sdk').Client;
+const unirest = require("unirest");
 const config = {
   channelAccessToken: 'dzaOAQEbS4W3KQFaoq2IbC8Z6rxrvk46MkI6tgcmhFRy9amJTG48myOZdg8OuKsex4aKxDgevUajHk9PgtXLR1GTjlFav5brcEKP8bV/o+YqkSeVylPHY+UtfzzNrZO4OT6ZGZSfa3cFvpNMosmuRQdB04t89/1O/w1cDnyilFU=',
   channelSecret: '6a5f9c0a5f70c92c3d64186f9a14ec16'
@@ -12,7 +13,14 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 const event = req.body.events[0];
   if (event.type === 'message' && event.message.type === 'text') {
     var message = event.source.userId +','+ event.message.text;
-        microgear.chat('node1', message);
+    var ALIAS = "node1";
+    var APPID = "Pocketbot/";
+    var KEY = "E8d0mBCaYxpb6FW";
+    var SECRET = "XxnxMl4kZ51vWCli1rQpEtib7";
+    var req = unirest("PUT", "https://api.netpie.io/microgear/" + APPID + ALIAS);
+    req.query({"auth": KEY + ":" + SECRET});
+    req.send(message);
+    req.end(function (res) {if (res.error) throw new Error(res.error);console.log(res.body);});
         console.log(message);
     }else{
       client.replyMessage(event.replyToken, {
@@ -25,48 +33,4 @@ return res.json({status: 'ok'})
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
-});
-//netpie microgear=========================================================================================
-var MicroGear = require('microgear');
-const APPID = 'PocketBot';
-const KEY = 'E8d0mBCaYxpb6FW';
-const SECRET = 'XxnxMl4kZ51vWCli1rQpEtib7';
-const ALIAS = 'nodejs';
-var microgear = MicroGear.create({ key : KEY, secret : SECRET, alias : ALIAS });
-microgear.connect(APPID);
-microgear.on('connected', function() { console.log('Connected to NETPIE');});
-// message handler
-microgear.on('message', function(topic,body) {
-  console.log('incoming : '+topic+' : '+body);
-  const equipment = ['D0','D1','D2','D3','D4','D5','D6','D7'];
-  var str = body.split(',');
-  var user = str[0];
-    console.log(user);
-  var pin = parseInt(str[1], 10);
-    console.log(pin.toString());
-  var state = str[2];
-    console.log(state);
-  var cmd = str[3];
-    console.log(cmd);
-  var msg = '';
-  var action = '';
-    if (cmd === 'check'){
-      if (state === 'on'){
-        msg = equipment[pin] + ' ถูกเปิดอยู่';
-        action = 'off';
-      }else if (state === 'off'){
-        msg = equipment[pin] + ' ถูกปิดอยู่';
-        action = 'on';
-      }
-      //create rich menu for user
-    }else{
-      if (cmd ==='on'){
-        msg = equipment[pin] +' ถูกเปิดแล้ว';
-      }else if(cmd === 'off'){
-        msg = equipment[pin] +' ถูกปิดแล้ว';
-      }else{
-        msg = "unknow command";
-      }
-    }
-    client.pushMessage('U7918f36feb5c4b9e4530b3d5ba88e274', { type: 'text',text: 'Hello'})
 });
