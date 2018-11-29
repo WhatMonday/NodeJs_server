fac51e55f934772100a7ed75020d9b66d6073e26
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-29 12:18:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ var action = '';
     client.createRichMenu({
            size: { width: 2500, height: 843 }, // Define size of rich menu
            selected: true, // Always display
-           name: 'test1', // rich menu name
-           chatBarText: 'test1', // show to user
+           name: 'button', // rich menu name
+           chatBarText: 'Button', // show to user
            areas: [ // Area and action of each boundary
                {
                    bounds: {
