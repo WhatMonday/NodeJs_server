5923640a4e3914561c02bd54bf879f78d607b6c4
WhatMonday <44560168+WhatMonday@users.noreply.github.com>
2018-11-29 14:51:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,13 +98,13 @@ var action = '';
     //delete al rich menu
     var richMenuList;
     client.getRichMenuList()
-    .then(async function(richMenuList){
-    richMenuList.forEach((list) => {
-      console.log(list.richMenuId)
-      var r = await client.unlinkRichMenuFromUser(USERID, list.richMenuId)
-      r = await client.deleteRichMenu(list.richMenuId)
-  })
-})
+    .then(function(richMenuList){
+      richMenuList.forEach((list) => {
+        console.log(list.richMenuId)
+        client.unlinkRichMenuFromUser(USERID, list.richMenuId)
+        .then (client.deleteRichMenu(list.richMenuId))
+      })
+    })
     //===================
   }
   client.pushMessage(USERID, { type: 'text',text: msg})
